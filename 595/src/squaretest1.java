dcc2c948b3d7de11e27c7e28faa8ebbb8c54c596
Babi Shiny <babishiny@Babis-Air.lan>
2023-01-30 01:13:06 -0800

    Configured Maven

---
+++ 595/src/squaretest1.java
@@ -0,0 +1,10 @@
+public class squaretest1 {
+
+		public int getSquare(int number) {
+			return number * number;
+		}
+
+		
+	}
+
+
---
+++ 595/src/squaretest123.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class helloTest1Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void add2numbers() {
+		var q = new squaretest1();
+		assertEquals(25, q.getSquare(5));
+	}
+
+
+}
