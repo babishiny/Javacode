da871e0d4083cff60120ce353a9b831df46b70fe
Babi Shiny <babishiny@Babis-Air.lan>
2023-02-10 05:43:41 -0800

    Added new project

---
+++ p595new/src/test/java/Testnow.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.*;
+
+public class Testnow {
+	
+	@Test
+	void display() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	void mars() {
+		System.out.println("Eat");
+	}
+	
+
+}
