fc66fc7c7081c3a2ad167f37286a26640af16baf
babishiny <babishiny@babis-air.lan>
2023-04-11 11:30:21 -0700

    Adding new file

---
+++ p595new/src/test/java/Testnow2.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.*;
+
+public class Testnow2 {
+	
+	@Test
+	void display() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	void mars() {
+		System.out.println("Eat");
+	}
+	
+
+}
