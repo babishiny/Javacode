64609715058ddda633ce73e5a15ce32cbbbb2b5e
Babi Shiny <babishiny@Babis-Air.lan>
2023-04-11 18:40:46 -0700

    Added new test cases

---
+++ p595new/src/test/java/Testnow4.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.*;
+
+public class Testnow4 {
+	
+	@Test
+	void display() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	void mars() {
+		System.out.println("Eat");
+	}
+	
+
+}
