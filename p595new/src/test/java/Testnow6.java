b3436ea04e120a30ee3bb9b5fcf8e9c74b147a69
Babi Shiny <babishiny@Babis-Air.lan>
2023-04-13 15:35:41 -0700

    Updated

--- p595new/src/test/java/Testnow6.java
+++ p595new/src/test/java/Testnow6.java
@@ -6,8 +6,7 @@ public class Testnow6 {
     @Test
     public void test_JUnit() {
         System.out.println("This is the testcase in this class");
-        String str1="CheckEqual";
+        String str1="Not to match";
         assertEquals("This is the testcase in this class", str1);
     }
 }
-
