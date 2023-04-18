7f0f1770f52d2ec0f40e9c8b34dcdc2664a2c040
babishiny11 <babishiny@gmail.com>
2023-04-17 20:44:37 -0700

    Added

---
+++ p595new/src/test/java/Testnow7.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.*;
+
+public class Testnow7 {
+	
+	@Test
+	void display() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	void mars() {
+		System.out.println("Eat");
+	}
+
+}
