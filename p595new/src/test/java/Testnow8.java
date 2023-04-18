9e114b1e92764302f5787d794346ada60088e05a
babishiny11 <babishiny@gmail.com>
2023-04-18 01:05:41 -0700

    Added new test case

---
+++ p595new/src/test/java/Testnow8.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.*;
+
+public class Testnow8 {
+	
+	@Test
+	void display() {
+		System.out.println("bye");
+	}
+	
+	@Test
+	void mars() {
+		System.out.println("Eat");
+	}
+
+}
