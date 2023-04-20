4ed288105bb45ee3e751a4857efc2a0a97aa8d74
babishiny11 <babishiny@gmail.com>
2023-04-20 16:54:20 -0700

    updated

--- p595new/src/test/java/Testnow5.java
+++ p595new/src/test/java/Testnow5.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.*;
 public class Testnow5 {
     @Test
     public void test_JUnit() {
-        System.out.println("This is the testcase in this class");
+        System.out.println("This is the updated test case");
         String str1="This is the testcase in this class";
-        assertEquals("This is the testcase in this class", str1);
+        assertEquals("This is the updated test case", str1);
     }
 }
