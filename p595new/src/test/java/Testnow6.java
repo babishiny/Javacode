313351ab9b2dce11eade2bf56bf0816e4926ca22
babishiny11 <babishiny@gmail.com>
2023-04-22 00:38:05 -0700

    updated

--- p595new/src/test/java/Testnow6.java
+++ p595new/src/test/java/Testnow6.java
@@ -6,7 +6,7 @@ public class Testnow6 {
     @Test
     public void test_JUnit() {
         System.out.println("This is the testcase in this class");
-        String str1="Not to match";
+        String str1="This is the testcase in this clas";
         assertEquals("This is the testcase in this class", str1);
     }
 }
