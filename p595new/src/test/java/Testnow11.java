844d0ba5f5ff752e7511782bbe9f45e7a8c83b53
babishiny11 <babishiny@gmail.com>
2023-04-22 14:33:08 -0700

    Added new test case

---
+++ p595new/src/test/java/Testnow11.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
+
+public class Testnow11 {
+    @Test
+    public void test_JUnit() {
+        System.out.println("Shape");
+        String str1="Color";
+        assertEquals("Shape", str1);
+    }
+}
