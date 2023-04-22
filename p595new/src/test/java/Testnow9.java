80d0487259cee3d2af79b83c9437d48804b8918a
babishiny11 <babishiny@gmail.com>
2023-04-22 14:35:59 -0700

    Updated

--- p595new/src/test/java/Testnow9.java
+++ p595new/src/test/java/Testnow9.java
@@ -6,7 +6,7 @@ public class Testnow9 {
     @Test
     public void test_JUnit() {
         System.out.println("A different failure");
-        String str1="A different failure mismatch";
+        String str1="A different failure";
         assertEquals("A different failure", str1);
     }
 }
