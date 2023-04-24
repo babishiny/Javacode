3ac11bdc5129a08961b3845554a37d8dee044554
babishiny11 <babishiny@gmail.com>
2023-04-24 02:33:22 -0700

    Updated

---
+++ p595new/src/main/java/now.java
@@ -0,0 +1,20 @@
+
+
+public class now {
+	
+
+	void display() {
+		System.out.println("bye");
+	}
+
+	void mars() {
+		System.out.println("Eat");
+	}
+	
+	public static void main (String[] reg) {
+		now it = new now();
+		it.display();
+		it.mars();
+	}
+
+}
