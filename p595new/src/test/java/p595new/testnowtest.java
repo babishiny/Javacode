9de4de05f67d1b61b83b9630c37979e79d14df63
babishiny11 <babishiny@gmail.com>
2023-04-24 10:43:49 -0700

    added

---
+++ p595new/src/test/java/p595new/testnowtest.java
@@ -0,0 +1,14 @@
+package p595new;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testnowtest {
+
+	@Test
+	void test() {
+		System.out.println("Eat");
+	}
+
+}
